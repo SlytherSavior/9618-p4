ac9a59825a14a89c1179315a4667187ad20058cc
Slyther Savior <shrijanpoudel9@gmail.com>
2024-07-23 21:30:40 +0545

    Try implementing stack in java without using OOP concepts

--- stack/stack.java
+++ stack/stack.java
@@ -1,9 +1,16 @@
-// stack implementation in java ! ! 
-class stackJava { 
-  
+// stack implementation in java ! Let's try without using oop concepts! 
 
+import java.util.Scanner ;
+
+class stackJava { 
+  public int maxLength; 
+  public int frontPointer; 
   // declare main method for java 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the max length of Stack");
+
+
     
   }
 
