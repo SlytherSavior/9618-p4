114d56e8ff0dcbadce5f85d081d583825f0b6b81
Slyther Savior <shrijanpoudel9@gmail.com>
2024-08-28 21:47:46 +0545

    defined functions, code initilalize function

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -1,19 +1,27 @@
-#So there are two ways to create a Linked List in python ! We will try the Object Oriented Programming ( OOP ) way here 
-
-# create a class for each item of linked list 
-
-class Node: 
-    def __init__(self): 
-        self.data = ""
+#node of a linked list
+class Node:
+    def __init__(self):
+        self.data = ''
         self.pointer = -1
 
-start_pointer = -1 
-free_pointer = 0  
+start_pointer =-1
+free_pointer = 0
 
-linked_list = [Node() for x in range(int(input("Enter the maximum number of elements in your linked list")))]
-        
-#now for the operations of linked_list
+linked_list = [Node() for i in range(int(input("Enter the size of the linkedlist you want")))]
 
-def add_node(): 
-    #a function to add a node 
-    print('hello world')
+def initialize():
+    global linked_list
+    for i in range(len(linked_list)-1):
+        linked_list[i].pointer = i+1
+    linked_list[len(linked_list)-1].pointer =-1
+
+initialize()
+
+#method to add element in linked list
+def addNode(element):
+   
+
+def printNodes():
+
+
+
