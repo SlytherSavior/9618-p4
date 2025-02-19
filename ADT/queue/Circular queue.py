3bd537ebab79c9022dd4ed09ee5a5432a9a9106a
Slyther Savior <shrijanpoudel9@gmail.com>
2025-02-19 14:18:52 +0545

    fix  : clear circular queue

--- ADT/queue/Circular queue.py
+++ ADT/queue/Circular queue.py
@@ -1,8 +1,1 @@
-class SaleData:
-    def __init__(self, saleidp, quantityp ) -> None:
-        pass
-        self.saleid = saleidp
-        self.quantity = quantityp
-
-mynode = SaleData("ABC", 2)
-print(mynode.quantity)
+#Circular queue implementation in python
