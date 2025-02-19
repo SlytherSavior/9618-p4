a61d6cafdfe97d4cc4feb849adede3b9dc51c3d6
Slyther Savior <shrijanpoudel9@gmail.com>
2025-02-19 11:57:37 +0545

    feat : add constructor

--- ADT/Binary_Tree/BinaryTree.java
+++ ADT/Binary_Tree/BinaryTree.java
@@ -1,10 +1,20 @@
 package ADT.Binary_Tree;
 
 public class BinaryTree {
+  public Integer leftPointer;
+  public String data; 
+  public Integer rightPointer; 
+
+  public BinaryTree(){
+    leftPointer = -1;
+    data = "";
+    rightPointer = -1;
+
+  }
   public static void main(String[] args) {
     
   }
 
-  
+
   
 }
